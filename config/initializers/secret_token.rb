7ea77061f76d913da46fc30bb9e88e2490b36744
Matthew Liu <matthewliu@gmail.com>
2011-10-05 00:49:13 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Koala2::Application.config.secret_token = '55eece39b274f2c369af8a936e180fec7439b42c3721123c71e1bf36b0ea4bdb305b94633704f77fc282064fd103aa4e0ff0600446389806cb0eec970fd69b37'
