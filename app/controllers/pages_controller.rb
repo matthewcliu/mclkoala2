7fb3e7aee3f7513d721a0bfdad44ca2c171730dc
Matthew Liu <matthewliu@gmail.com>
2011-10-19 17:18:18 -0700

    Experiment with koala JS SDK

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,7 @@ class PagesController < ApplicationController
   end
 
   def fakeauth
-    @graph = Koala::Facebook::API.new("AAACEdEose0cBAHHC94vJh1qDceUvCoBruMsB0yumV5qJIUjJDGGNE5AMcPQaqCvVy3fb6eOZBoA8VHQmZBjzhOQuZAkWHmAHGZBqO1qFIgZDZD")
+    @graph = Koala::Facebook::API.new("AAAD9FnjdGZAABADiesCVaffYjUqLzFMUegFVJyThS5VCN8FDMHkcOqLoiGtUewZADZCoVuY8n12eGufcPkODayBmubNUJcprhxEStMxagZDZD")
     @likes = @graph.get_connections("matthewliu", "likes")
   end
 
@@ -18,9 +18,8 @@ class PagesController < ApplicationController
     @oauth = Koala::Facebook::OAuth.new('278272958863760', '5a614013838d3a0543de6b30a4b62b35', 'http://bday-reminder.com/pages/realauth')
     @fb_user = @oauth.get_user_info_from_cookies(cookies)
     @fb_user_id = @oauth.get_user_from_cookies(cookies)
-    #auth_token = @fb_user[:access_token]
-    #@graph = Koala::Facebook::API.new(auth_token)
-    #@likes = @fb_user.get_connections("me", "likes")
+    @graph = Koala::Facebook::API.new(@fb_user['access_token'])
+    @likes = @graph.get_connections("me", "likes")
   end
   
   def example
