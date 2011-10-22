38184742eeae3532ca56f8c844c70d9f115c29df
Matthew Liu <matthewliu@gmail.com>
2011-10-21 18:25:54 -0700

    Fixed YAML file

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,1 +1,23 @@
-FB_OPTIONS = YAML.load_file("#{RAILS_ROOT}/config/facebook.yml")[RAILS_ENV]
+fb_options = YAML.load_file(Rails.root.join("config", "facebook.yml"))[:development]
+
+puts "*"*200
+puts YAML.load_file(Rails.root.join("config", "facebook.yml"))
+
+puts "*"*200
+
+
+puts Rails.root.join("config", "facebook.yml")
+
+puts "*"*200
+
+
+puts fb_options
+
+puts "*"*200
+
+
+$app_id = fb_options['app_id']
+$secret_key = fb_options['app_id']
+$callback_url = fb_options['app_id']
+
+puts $app_id
